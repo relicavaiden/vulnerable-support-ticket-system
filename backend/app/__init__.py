4adec123cd9be7d2954887b4b85761937fd74c57
Edward (Dirrick) Ricks <dirrickricks@gmail.com>
2026-07-18 00:16:59 -0500

    Add database initialization helper

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -1,8 +1,18 @@
+import os
+
+
 from flask import Flask
+
 from app.routes.health import health_bp
 
 def create_app():
     app = Flask(__name__)
+    app.config["DATABASE"] = os.path.join(app.instance_path, "vulnerable_ticket_system.db")
+
+    os.makedirs(app.instance_path, exist_ok=True)
+
+    from . import db
+    db.init_app(app)
     app.register_blueprint(health_bp)
 
     return app
