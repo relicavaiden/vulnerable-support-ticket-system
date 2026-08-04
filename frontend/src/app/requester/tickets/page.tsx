c2a33fb150e9bfb4e8318276e15942ada1874463
Edward (Dirrick) Ricks <dirrickricks@gmail.com>
2026-08-03 20:47:20 -0500

    feat: protect role ticket pages

--- frontend/src/app/requester/tickets/page.tsx
+++ frontend/src/app/requester/tickets/page.tsx
@@ -1,4 +1,39 @@
+"use client"
+
+import { useEffect, useState } from "react";
+import { useRouter } from "next/navigation";
+
+import { getCurrentUser } from "@/lib/api"
+
 export default function RequesterTicketsPage() {
+
+    const [isLoading, setIsLoading] = useState(true);
+    const router = useRouter();
+
+    useEffect(() => {
+        async function checkAuth() {
+            try {
+                const data = await getCurrentUser();
+
+                if (data.user.role !== "requester") {
+                    router.replace("/resolver/tickets");
+                    return;
+                }
+
+                setIsLoading(false);
+            } catch {
+                router.push("/login");
+            }
+        }
+
+        checkAuth();
+    }, [router]);
+
+    if (isLoading) {
+        return <main>Loading...</main>
+    }
+
+
     return (
         <main>
             <h1>Requester Tickets</h1>
--- frontend/src/app/resolver/tickets/page.tsx
+++ frontend/src/app/resolver/tickets/page.tsx
@@ -1,4 +1,37 @@
+"use client";
+
+import { useEffect, useState } from "react";
+import { useRouter } from "next/navigation";
+
+import { getCurrentUser } from "@/lib/api";
+
 export default function ResolverTicketsPage() {
+    const [isLoading, setIsLoading] = useState(true);
+    const router = useRouter();
+
+    useEffect(() => {
+        async function checkAuth() {
+            try {
+                const data = await getCurrentUser();
+
+                if (data.user.role !== "resolver") {
+                    router.replace("/requester/tickets");
+                    return;
+                }
+
+                setIsLoading(false);
+            } catch {
+                router.replace("/login")
+            }
+        }
+
+        checkAuth();
+    }, [router]);
+
+    if (isLoading) {
+        return <main>Loading...</main>;
+    }
+    
     return (
         <main>
             <h1>Resolver Tickets</h1>
